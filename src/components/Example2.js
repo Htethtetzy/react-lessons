41d8174a0de69a78306ecda448d96b9a56d16674
Htethtetzy <157711449+Htethtetzy@users.noreply.github.com>
2025-05-29 15:01:25 +0630

    update

---
+++ src/components/Example2.js
@@ -0,0 +1,10 @@
+"use client";
+import react from "react";
+import React from "react";
+export default function Person() {
+return(
+    <div>
+        <h1>I am Function Component.</h1>
+    </div>
+);
+}
